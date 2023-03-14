d4cb5a8e3235b0675f8f1b34c57c509ff6a64818
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-14 14:46:34 +0100

    14.03

--- dist/detections.js
+++ dist/detections.js
@@ -12,3 +12,48 @@ function provideId(id) {
     }
     id.toLowerCase();
 }
+function isAdmin(account) {
+    if ('isAdmin' in account) {
+        return account.isAdmin;
+    }
+}
+// Instanceof narrowing
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return 'fish food';
+    }
+    else {
+        pet;
+        return 'bird food';
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === 'circle') {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case 'circle':
+            return Math.PI * shape.radius ** 2;
+            break;
+        case 'square':
+            return shape.side * shape.side;
+        default:
+            const _defaultForShape = shape;
+            return _defaultForShape;
+    }
+}
--- src/detections.ts
+++ src/detections.ts
@@ -12,3 +12,85 @@ function provideId(id: string | null) {
     }
     id.toLowerCase();
 }
+
+interface User {
+    name: string,
+    email: string
+}
+
+interface Admin {
+    name: string,
+    email: string,
+    isAdmin: boolean
+}
+
+function isAdmin(account: User | Admin) {
+    if ('isAdmin' in account) {
+        return account.isAdmin;
+    }
+}
+
+// Instanceof narrowing
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return 'fish food';
+    } else {
+        pet
+        return 'bird food';
+    }
+}
+
+interface Circle {
+    kind: 'circle'
+    radius: number
+}
+
+interface Square {
+    kind: 'square'
+    side: number
+}
+
+interface Rectangle {
+    kind: 'rectangle',
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+    if (shape.kind === 'circle') {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+    switch(shape.kind) {
+        case 'circle':
+            return Math.PI * shape.radius ** 2;
+            break;
+        case 'square':
+            return shape.side * shape.side;
+        default:
+            const _defaultForShape: never = shape;
+            return _defaultForShape;
+    }
+}
