12bd8923f10a05698d58ef0217b94262b7321274
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-14 10:46:02 +0100

    14.03

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,34 @@
 // Classes
 class User {
     constructor(email, name) {
+        this._courseCount = 1;
         this.city = 'Opole';
         this.email = email;
         this.name = name;
+    }
+    deleteToken() {
+        console.log('Token deleated');
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNumber;
+    }
+}
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const emilia = new User('emilia@wp.pl', 'emilia');
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 // Classes
 class User {
-    public email: string
+    protected _courseCount = 1
+    email: string
     name: string
-    private readonly city: string = 'Opole'
+    readonly city: string = 'Opole'
     constructor(email: string, name: string) {
         this.email = email;
         this.name = name;
+    }
+    private deleteToken() {
+        console.log('Token deleated');
+        
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`;
+    }
+
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+    set courseCount(courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNumber;
+    }
+ 
+}
+
+class SubUser extends User {
+    isFamily: boolean = true;
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 
